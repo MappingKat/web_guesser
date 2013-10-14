85da9ae89886abb9be46ed7b98365739ce81dd58
Katrina Engelsted <katrina@engelsted.co>
2013-10-14 14:01:53 -0600

    I0: firsst sinatra server

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+secret = (1 + rand(99)).to_s
+puts secret
+
 get '/' do
-    rand(99)
-    "Your lucky number is #{rand(99)}"
+  final_guess = false
+
+  if secret.to_i + 20 < params["guess"].to_i 
+    guess = "Damn....Way TOO HIGH"
+  elsif secret.to_i - 20 > params["guess"].to_i 
+    guess = "Waaaay... TOO Low"
+  elsif secret.to_i > params["guess"].to_i
+    guess = "...too Low"
+  elsif secret.to_i < params["guess"].to_i
+    guess = "...too High"
+  elsif secret.to_i == params["guess"].to_i
+    guess = "Ding! Ding! You got it! Ding! Ding!"
+    final_guess = true
+  end
+
+  if final_guess == true
+    right_or_wrong = true
+  end
+
+  erb :index, :locals => {:number => secret, :guess => guess, :correct => final_guess, :right_or_wrong => right_or_wrong}
 end
