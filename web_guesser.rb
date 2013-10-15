faa2fdb6e30e5763bc13c2749b722341c0549c62
Katrina Engelsted <katrina@engelsted.co>
2013-10-14 19:58:15 -0600

    working but needs test and refactoring

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,20 +2,38 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(101).to_i
+@color = 'white'
 
-get '/' do
-guess = params["guess"].to_i
+def color
+  end
 
+def check_guess(guess)
   if SECRET_NUMBER + 20 < guess
+    @color = 'red'
     message = "Damn....Way TOO HIGH"
   elsif SECRET_NUMBER - 20 > guess
+    @color = 'navy'
     message = "Waaaay... TOO Low"
   elsif SECRET_NUMBER > guess
     message = "...too Low"
+    @color = 'blue'
   elsif SECRET_NUMBER < guess
     message = "...too High"
+    @color = 'pink'
   elsif SECRET_NUMBER == guess
     message = "Ding! Ding! You got it! Ding! Ding!"
+    @color = 'green'
   end
-  erb :index2, :locals => {:number => SECRET_NUMBER, :message => message}
+  message
+end
+
+get '/' do
+  guess = params["guess"]
+  if guess
+    message = check_guess(guess.to_i)
+  else
+    message = 'Pick a number between 1 and 100'
+  end
+  
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => @color}
 end
